e7d2a329d3fea36d2bd7a543add28657bcde4b0d
Alexandr Krasovsky <krasovskii.av@progressufa.ru>
2019-11-26 09:27:14 +0500

    Inheritance: add error

--- inheritance/main.cpp
+++ inheritance/main.cpp
@@ -1,8 +1,33 @@
 #include <QCoreApplication>
+#include <QDebug>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+class A {
+public:
+	int _a = 1;
+};
+class B : public A {
+public:
+	int _b = 2;
+};
+
+class Abstraciya : public map<int, A*> {
+public:
+	Abstraciya(const map<int, B*>& b) { qDebug() << b.at(0)->_b; }
+};
+
+void func(const map<int, A*> is_a)
+{
+	qDebug() << is_a.at(0)->_a;
+}
+int main(int argc, char* argv[])
 {
 	QCoreApplication a(argc, argv);
-
+	B _is_b;
+	map<int, B*> _b1;
+	_b1[0] = &_is_b;
+	Abstraciya(_b1);
+	func(Abstraciya(_b1));
 	return a.exec();
 }
